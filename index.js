edb7f63839f31e65006fc2a1e70cbf5522d2b274
atacoiguess <bratleyaston@gmail.com>
2022-07-22 12:34:08 +1000

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+function test() {
+    require("kahoot.js-updated");
+}
+
+var script = document.createElement("script");
+script.async;
+script.type = "module";
+script.innerText = test;
+document.body.appendChild(script);
